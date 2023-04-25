d859e380455ca28c85f3622a96011ca4195c83dd
Fatimazohra95 <fatimazohra32@gmail.com>
2023-04-25 14:23:53 -0700

    Task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *jack_bver - Prints every minute of the day of Jack Baver
+ *jack_bauer - Prints every minute of the day of Jack Baver
  *
  *Return: ...
  */
-void jack_baver(void)
+void jack_bauer(void)
 {
 	int a, b, c, d;
 
